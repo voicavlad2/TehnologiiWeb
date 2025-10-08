d92027ac90db742c2d1b134a074afeaadc8c1e9b
Vlad <vladvoica2@gmail.com>
2025-10-08 18:15:21 +0300

    added difference counter for strings

--- functions.js
+++ functions.js
@@ -4,4 +4,32 @@ const sayHello = (name) => `Hello ${name}`;
 
 const greet = (names) => `Hello ${names.join(" ")}`;
 
-console.log(greet(["Florin", "Liviu", "Sandu"]));
+// console.log(greet(["Florin", "Liviu", "Sandu"]));
+
+function checkDivision(n,divisor){
+    if(n % divisor){
+        return false;
+    }else{
+        return true;
+    } 
+}
+
+// console.log(checkDivision(20,3));
+// console.log(checkDivision(20,2));
+
+function countDif(str1, str2){
+    let count = 0;
+    if(str1.length !== str2.length){
+        return -1;
+    } else{
+        for(let i = 0; i < str1.length; i++){
+            if(str1[i] !== str2[i])
+                count++;
+        }
+        return count;
+    }
+}
+
+console.log(countDif("hop", "hop"));   // 1
+console.log(countDif("hopa", "popa")); // 2
+console.log(countDif("Bam", "PamPam"));  // 3
